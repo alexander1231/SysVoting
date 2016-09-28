f659cfd65284be01d950847783563c73a9a5464d
ZodiacFireworks <martin.vuelta@gmail.com>
2016-09-28 07:09:07 -0500

    Corregido el error que originaba un 'unmatching answer'

--- voting_poll/views.py
+++ voting_poll/views.py
@@ -6,49 +6,60 @@ from voting_poll.models import VotingPoll
 
 
 class HomeView(TemplateView):
-	template_name = "home.html"
+    template_name = "home.html"
 
-	def get(self, request, *args, **kwargs):
-		context = self.get_context_data(*args, **kwargs)
-		context["Polls"] = VotingPoll.objects.all()
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(*args, **kwargs)
+        context["Polls"] = VotingPoll.objects.all()
 
-		return self.render_to_response(context)
+        return self.render_to_response(context)
 
 
 class DetailView(TemplateView):
-	template_name = "detail.html"
+    template_name = "detail.html"
 
-	def get(self, request, *args, **kwargs):
-		context = self.get_context_data(*args, **kwargs)
-		show_stats = False
-		
-		poll = get_object_or_404(VotingPoll, question__slug=kwargs.get('slug', ''))
-		poll_id = poll.uuid.hex
-		
-		if request.COOKIES.get(poll_id, None) is not None:
-			show_stats = True
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(*args, **kwargs)
+        show_stats = False
 
-		context["Poll"] = poll
-		context["ShowStats"] = show_stats
+        poll = get_object_or_404(VotingPoll, question__slug=kwargs.get('slug', ''))
+        poll_id = poll.uuid.hex
 
-		return self.render_to_response(context)
+        if request.COOKIES.get(poll_id, None) is not None:
+            show_stats = True
 
-	def post(self, request, *args, **kwargs):
-		context = self.get_context_data(*args, **kwargs)
+        context["Poll"] = poll
+        context["ShowStats"] = show_stats
 
-		try:
-			poll = get_object_or_404(VotingPoll, question__slug=kwargs.get('slug', ''))
-			answer_id = int(request.POST.get('answer')[0])
-			answer = poll.question.answers.get(id=answer_id)
-			answer.count = answer.count + 1;
-			answer.save()
-		except Exception as msg:
-			raise Exception(msg)
+        return self.render_to_response(context)
 
-		context["Poll"] = poll
-		context["Success"] = True
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(*args, **kwargs)
 
-		response = self.render_to_response(context)
-		response.set_cookie(poll.uuid.hex, 'true', max_age = 5000000000) 
+        try:
+            poll = get_object_or_404(VotingPoll, question__slug=kwargs.get('slug', ''))
 
-		return response
+            # print(request.POST)
+            # print(poll.question.answers.all().values('id'))
+            # print(poll.question.answers.all())
+
+            # print(request.POST.get('answer'))
+            # print(type(request.POST.get('answer')))
+
+            # El [0] esta demás u.u
+            # el get te devuelve el único valor en la lista de
+            # answer_id = int(request.POST.get('answer')[0])
+            answer_id = int(request.POST.get('answer'))
+            answer = poll.question.answers.get(id=answer_id)
+            answer.count = answer.count + 1
+            answer.save()
+        except Exception as msg:
+            raise Exception(msg)
+
+        context["Poll"] = poll
+        context["Success"] = True
+
+        response = self.render_to_response(context)
+        response.set_cookie(poll.uuid.hex, 'true', max_age=5000000000)
+
+        return response
